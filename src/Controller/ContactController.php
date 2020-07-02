0515fdc1747f7858b9d771b139053b3074cb1500
2maj <adjimahamat@gmail.com>
2020-07-02 21:41:07 +0200

    Page Contact: contacter nous, config mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -20,20 +20,20 @@ class ContactController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
 
+
             // On crée le message
             $message = (new \Swift_Message('Nouveau contact'))
                 // On attribue l'expéditeur
                 ->setFrom($contact['email'])
                 // On attribue le destinataire
-                ->setTo('contact@tropicshops.fr')
+                ->setTo("contact@tropicshop.fr" )
                 // On crée le texte avec la vue
                 ->setBody(
-                    $this->renderView(
-                        'emails/contact.html.twig', compact('contact')
-                    ),
-                    'text/html'
-                )
-            ;
+                  $this->renderView(
+                      'emails/contact.html.twig', compact('contact')
+                  ),
+                  'text/html'
+                );
             $mailer->send($message);
 
             $this->addFlash('success', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.'); // Permet un message flash de renvoi
