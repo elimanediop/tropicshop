1224c4c59da3f5e27ed2a68a432409e9b20bf180
2maj <adjimahamat@gmail.com>
2020-07-13 21:27:21 +0200

    Maps: rendre indépendant les input adresse

--- public/js/auto-complete.js
+++ public/js/auto-complete.js
@@ -1,17 +1,19 @@
-var inputLocation = document.querySelector("[data-location]");
-var defaultBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(48.8566667, 2.3509871));
+function autocomp(input) {
+    var inputLocation = input;
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(48.8566667, 2.3509871));
 
-var options = {
-    bounds: defaultBounds,
-    types: []
-};
+    var options = {
+        bounds: defaultBounds,
+        types: []
+    };
 
-var autocomplete = new google.maps.places.Autocomplete(inputLocation, options);
-autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-        window.alert("No details available for input: '" + place.name + "'");
-        return;
-    }
-});
+    var autocomplete = new google.maps.places.Autocomplete(inputLocation, options);
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+    });
+}
