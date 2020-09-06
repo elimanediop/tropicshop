326fd2ad77dde7cc237aee802f406474d4d68f62
2maj <adjimahamat@gmail.com>
2020-09-06 20:49:29 +0200

    Migration des modif form

---
+++ migrations/Version20200906122207.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200906122207 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE lat lat DOUBLE PRECISION NOT NULL, CHANGE lon lon DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE lat lat DOUBLE PRECISION DEFAULT NULL, CHANGE lon lon DOUBLE PRECISION DEFAULT NULL');
+    }
+}
