447cb0d2c71618f5aea7ec96018567a0a59a9ef3
Moussa <adjimahamat@gmail.com>
2020-11-07 18:30:13 +0100

    Fix problème de migration en prod

--- migrations/Version20201105172833.php
+++ migrations/Version20201105172833.php
@@ -23,7 +23,6 @@ final class Version20201105172833 extends AbstractMigration
         $this->addSql('DROP TABLE produit_store_origine');
         $this->addSql('ALTER TABLE produit_store ADD origine_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE produit_store ADD CONSTRAINT FK_CFCB3FE387998E FOREIGN KEY (origine_id) REFERENCES origine (id)');
-        $this->addSql('CREATE INDEX IDX_CFCB3FE387998E ON produit_store (origine_id)');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +32,6 @@ final class Version20201105172833 extends AbstractMigration
         $this->addSql('ALTER TABLE produit_store_origine ADD CONSTRAINT FK_225D315687998E FOREIGN KEY (origine_id) REFERENCES origine (id) ON UPDATE NO ACTION ON DELETE CASCADE');
         $this->addSql('ALTER TABLE produit_store_origine ADD CONSTRAINT FK_225D3156998D9A02 FOREIGN KEY (produit_store_id) REFERENCES produit_store (id) ON UPDATE NO ACTION ON DELETE CASCADE');
         $this->addSql('ALTER TABLE produit_store DROP FOREIGN KEY FK_CFCB3FE387998E');
-        $this->addSql('DROP INDEX IDX_CFCB3FE387998E ON produit_store');
         $this->addSql('ALTER TABLE produit_store DROP origine_id');
     }
 }
