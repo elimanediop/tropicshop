7790f407b104d891fcbe11b818f6108c73d7270a
Moussa <adjimahamat@gmail.com>
2020-11-07 18:43:13 +0100

    Fix migration table inutile

--- migrations/Version20201105172833.php
+++ migrations/Version20201105172833.php
@@ -20,7 +20,6 @@ final class Version20201105172833 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE produit_store_origine');
         $this->addSql('ALTER TABLE produit_store ADD origine_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE produit_store ADD CONSTRAINT FK_CFCB3FE387998E FOREIGN KEY (origine_id) REFERENCES origine (id)');
     }
@@ -28,9 +27,6 @@ final class Version20201105172833 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE produit_store_origine (produit_store_id INT NOT NULL, origine_id INT NOT NULL, INDEX IDX_225D315687998E (origine_id), INDEX IDX_225D3156998D9A02 (produit_store_id), PRIMARY KEY(produit_store_id, origine_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE produit_store_origine ADD CONSTRAINT FK_225D315687998E FOREIGN KEY (origine_id) REFERENCES origine (id) ON UPDATE NO ACTION ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE produit_store_origine ADD CONSTRAINT FK_225D3156998D9A02 FOREIGN KEY (produit_store_id) REFERENCES produit_store (id) ON UPDATE NO ACTION ON DELETE CASCADE');
         $this->addSql('ALTER TABLE produit_store DROP FOREIGN KEY FK_CFCB3FE387998E');
         $this->addSql('ALTER TABLE produit_store DROP origine_id');
     }
