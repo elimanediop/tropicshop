2e7ea601693fd9775e580055b0847f6047fa44c0
2maj <adjimahamat@gmail.com>
2020-12-04 19:40:24 +0100

    Modification des fichiers de migrations pour réduire les erreurs

--- migrations/Version20201106205915.php
+++ migrations/Version20201106205915.php
@@ -21,7 +21,7 @@ final class Version20201106205915 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE stock (id INT AUTO_INCREMENT NOT NULL, produit_store_id INT NOT NULL, quantity INT DEFAULT NULL, INDEX IDX_4B365660998D9A02 (produit_store_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE stock ADD CONSTRAINT FK_4B365660998D9A02 FOREIGN KEY (produit_store_id) REFERENCES produit_store (id)');
+        //$this->addSql('ALTER TABLE stock ADD CONSTRAINT FK_4B365660998D9A02 FOREIGN KEY (produit_store_id) REFERENCES produit_store (id)');
     }
 
     public function down(Schema $schema) : void
