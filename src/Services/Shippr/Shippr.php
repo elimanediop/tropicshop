a1f65fd54fd5e0df90687d66c281eed04b25adaf
2maj <adjimahamat@gmail.com>
2021-01-09 23:49:34 +0100

    Shippr api: les autres methodes et tests

--- src/Services/Shippr/Shippr.php
+++ src/Services/Shippr/Shippr.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
 class Shippr
 {
-    private const KEYAPI_PROD = "9cb61c81534909b3e4c5d7651af7e3c011f72d15ca6bed35d016aa6a7224003029146aa86e92ac36b140b82a823dd032";
+    private const KEYAPI_PROD = "3d3bcffc3139e3aae5e1219468b143842cfc67959b14d10d407ca296d91512fc1db655d8314193cbb3cad11624b48ea8";
     private const URLAPI_PROD = "https://api.shippr.io/v1";
     public const CREATED = "CREATED";
     public const ASSIGNED = "ASSIGNED";
@@ -71,52 +71,92 @@ class Shippr
     public function createDelivery($time_window_start, $time_window_end,
                                    $desired_pickup_time, $has_retrun)
     {
+        //'{"time_window_start": "2021-01-24T14:15:22Z","time_window_end": "2021-01-24T14:15:22Z","desired_pickup_time": "2021-01-18T04:20:00.000+02:00","pickup_location": {"first_name": "Anduin","last_name": "Wrynn","company": "Kenobi, Skywalker and Solo food products","phone": "0499422942","address": "Abbey Road 1, Saint-Gilles, Belgium","comment": "The princess is in another castle","email": "recipient@example.com"},"destinations": [{"location": {"first_name": "Anduin","last_name": "Wrynn","company": "Kenobi, Skywalker and Solo food products","phone": "0499422942","address": "Abbey Road 1, Saint-Gilles, Belgium","comment": "The princess is in another castle","email": "recipient@example.com"},"package_type": "SMALL","reference_number": "string","time_window_start": "2019-08-24T14:15:22Z","time_window_end": "2019-08-24T14:15:22Z"}],"has_return": true}';
         $data_params = '{
-  "time_window_start": "2019-08-24T14:15:22Z",
-  "time_window_end": "2019-08-24T14:15:22Z",
-  "desired_pickup_time": "2018-07-18T04:20:00.000+02:00",
-  "pickup_location": {
-    "first_name": "Anduin",
-    "last_name": "Wrynn",
-    "company": "Kenobi, Skywalker and Solo food products",
-    "phone": "0499422942",
-    "address": "Abbey Road 1, Saint-Gilles, Belgium",
-    "comment": "The princess is in another castle",
-    "email": "recipient@example.com"
-  },
-  "destinations": [
-    {
-      "location": {
-        "first_name": "Anduin",
-        "last_name": "Wrynn",
-        "company": "Kenobi, Skywalker and Solo food products",
-        "phone": "0499422942",
-        "address": "Abbey Road 1, Saint-Gilles, Belgium",
-        "comment": "The princess is in another castle",
-        "email": "recipient@example.com"
-      },
-      "package_type": "SMALL",
-      "reference_number": "string",
-      "time_window_start": "2019-08-24T14:15:22Z",
-      "time_window_end": "2019-08-24T14:15:22Z"
-    }
-  ],
-  "has_return": true
-}';
+            "time_window_start": "2021-01-24T14:15:22Z",
+            "time_window_end": "2021-01-24T14:15:22Z",
+            "desired_pickup_time": "2021-01-18T04:20:00.000+02:00",
+            "pickup_location": {
+            "first_name": "Anduin",
+            "last_name": "Wrynn",
+            "company": "Kenobi, Skywalker and Solo food products",
+            "phone": "0499422942",
+            "address": "Abbey Road 1, Saint-Gilles, Belgium",
+            "comment": "The princess is in another castle",
+            "email": "recipient@example.com"
+            },
+            "destinations": [
+            {
+              "location": {
+                "first_name": "Anduin",
+                "last_name": "Wrynn",
+                "company": "Kenobi, Skywalker and Solo food products",
+                "phone": "0499422942",
+                "address": "Abbey Road 1, Saint-Gilles, Belgium",
+                "comment": "The princess is in another castle",
+                "email": "recipient@example.com"
+              },
+              "package_type": "SMALL",
+              "reference_number": "string",
+              "time_window_start": "2019-08-24T14:15:22Z",
+              "time_window_end": "2019-08-24T14:15:22Z"
+            }
+            ],
+            "has_return": true
+        }';
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $this::URLAPI_PROD.'/deliveries');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_params);
+
+
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = "Authorization: Bearer ".$this::KEYAPI_PROD;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        return $result;
+    }
+
+    public function getDelivery(string $uuid)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this::URLAPI_PROD."/deliveries/{$uuid}");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "time_window_start=$time_window_start".
-            "&time_window_end=$time_window_end".
-            "&desired_pickup_time=$desired_pickup_time".
-            "&pickup_location={}".
-            "&destinations=[]".
-            "&has_return=$has_retrun"
-        );
+
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = "Authorization: Bearer ".$this::KEYAPI_PROD;
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        return $result;
+    }
+
+    public function cancelDelivery(string $uuid)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $this::URLAPI_PROD."/deliveries/{$uuid}/actions/cancel");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 
 
         curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
--- tests/Shippr/ShipprTest.php
+++ tests/Shippr/ShipprTest.php
@@ -18,7 +18,24 @@ class ShipprTest extends TestCase
     public function testDeliveries(){
         $shippr = new Shippr();
         $deliveries = $shippr->getDeliveries(10, Shippr::CREATED);
-        $this->assertEquals(0, count(json_decode($deliveries)->data), "Get health API");
+        $this->assertEquals(0, count(json_decode($deliveries)->data), "Get all deliveries API");
     }
 
+    public function testCreateDelivery(){
+        $shippr = new Shippr();
+        $delivery = $shippr->createDelivery(1,1,1,5);
+        $this->assertEquals(true, count(json_decode($delivery)->has_return), "Get health API");
+    }
+
+    public function testGetSingleDelivery(){
+        $shippr = new Shippr();
+        $delivery = $shippr->getDelivery("a8-dhi");
+        $this->assertEquals(true, count(json_decode($delivery)->has_return), "Get single delivery API");
+    }
+
+    public function testCancelDelivery(){
+        $shippr = new Shippr();
+        $delivery = $shippr->cancelDelivery("a8-dhi");
+        $this->assertEquals(true, $delivery, "Canceled ! delivery API");
+    }
 }
